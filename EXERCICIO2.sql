6d93fec87d4a7bff1382322423f78d7260b459a3
Iago Rodrigues <102769741+iagoarodrigues@users.noreply.github.com>
2022-09-20 20:18:27 -0300

    Update EXERCICIO2.sql

--- EXERCICIO2.sql
+++ EXERCICIO2.sql
@@ -1,8 +1,10 @@
--- BDIII AULA2
+-- BDIIIA02
 
+DROP DATABASE bd3;
 CREATE DATABASE bd3;
 
 USE bd3;
+
 -- DROP TABLE IF EXISTS alunos;
 
 -- create table
@@ -20,79 +22,40 @@ insert into alunos (nome, idade, nascimento) values ('fred', 13, '2008-05-12');
 insert into alunos (nome, idade, nascimento) values ('joao', null, null);
 insert into alunos (nome, idade, nascimento) values ('joao', 14, '2007-07-05');
 
--------------------
 
--- Utilizando a estrutura acima faça as seguintes consultas:
+select * from alunos;
 
+-- atualizar nome para Elias e idade para 40 do registro com id = 1
+/*
+UPDATE NomeDaTabela
+SET coluna1 = 'valor',
+coluna2 = 'valor'
+WHERE campo3 = 'valor';
+*/
 
--- 1) Selecione todos os registros da tabela alunos
-	select * from alunos
+UPDATE alunos
+SET nome = "Elias", idade = 20
+WHERE id = 1;
 
--- 2) Retorne somente o nome e a idade das linhas que não tenham a idade nula
-	select * from alunos
-	where nome
-    and
-    nascimento is not null;
-        
+-- excluir o registro com id = 1 da tabela alunos
+/*
+DELETE FROM NomeDaTabela
+WHERE coluna = 'valor';
+*/
 
--- 3) Retorne todos os registros em ordem decrescente de nome
-	select * from alunos
-    order by nome desc;
+DELETE FROM alunos 
+WHERE id = 1;
 
--- 4) Retorne o nome e a idade de somente dois registros
-	select * from alunos
-    limit 2;
+-- operadores lógicos
 
--- 5) Atualize o nome do registro com id = 1 para "Samuel"
-	update alunos
-    set nome = 'Samuel' 
-    where id = 1;
-    
--- 6) Exclua o registro com id = 3
-	delete from alunos
-    where id = 1,2,3;
+-- selecione os registro com nome de joao e idade igual a 14
+SELECT * FROM alunos
+WHERE nome='joao' AND idade=14;
 
--- 7) Selecione os registros que tenham o nome = 'Samuel' ou idade = 14
-	select * from alunos
-    where nome='Samuel' or idade=14;
+-- selecionar todos os registros que tenham o nome igual a joao ou idade igual a 11
+SELECT * FROM alunos
+WHERE nome='joao' OR idade=10;
 
--- 8) Selecione os registros que tenham o nome = 'joao' e idade = 14
-	select * from alunos
-    where nome='João' or idade=14;
-
--- 9) Selecione os registros que não possuem o nome = 'joao'
-	select * from alunos
-    where not name='joao';
-
--- 10) Conte todas as idades da tabela
-	select count(idade)
-	from alunos;
--- 11) Recupere a média da idade dos alunos
-	select avg(idade)
-    from alunos;
-   
--- 12) Some a idade dos alunos
-	select sum(idade)
-    from alunos;
- 
--- 13) Selecione todos os registros que tenham em algum lugar do nome a string 'o'
-	select * from  alunos
-    where nome like '%o%';
-
--- 14) Selecione os registro com idade entre 10 e 12 anos
-	select * from alunos
-    where idade between 10 and 12;
-
-
--- 15) Selecione os registro com idade fora da faixa de 10 e 12 anos
-	select * from alunos
-    where idade not between 10 and 12;
-
--- 16) Selecione os alunos com data de nascimento entre '2008-05-12' e '2011-03-04'
-	select * from alunos
-    where idade between '2008-05-12' and '2011-03-04';
-
--- 17) Retorne o nome usando o alias nome_do_cliente
-	select nome as nome_do_cliente
-    from alunos;
-
+-- selecionar todos os registros que não tenham o nome igual a joao
+SELECT * FROM alunos
+WHERE NOT nome='joao';

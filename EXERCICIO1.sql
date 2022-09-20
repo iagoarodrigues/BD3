a189ef33a3e77df48fe0debb65094f37b421ec01
Iago Rodrigues <102769741+iagoarodrigues@users.noreply.github.com>
2022-09-20 20:16:55 -0300

    Update EXERCICIO1.sql

--- EXERCICIO1.sql
+++ EXERCICIO1.sql
@@ -1,51 +1,52 @@
--- BDIII AULA2
+-- BDIIIA01
 
-use bd3;
-dsdas
-create table alunos (
-	id int(11) primary key auto_increment,
-    nome varchar(255) not null,
-    nascimento date default null
+DROP DATABASE bd3;
+CREATE DATABASE bd3;
+
+USE bd3;
+
+-- DROP TABLE IF EXISTS alunos;
+
+-- create table
+CREATE TABLE alunos (
+	id INT AUTO_INCREMENT primary key,
+	nome varchar (50),
+	idade int (3),
+    nascimento date DEFAULT NULL
 );
 
-insert into alunos (nome, nascimento) values ('leo', '2011-03-04');
-insert into alunos (nome, nascimento) values ('juju', '2010-10-08');
-insert into alunos (nome, nascimento) values ('fred', '2008-05-12');
-insert into alunos (nome, nascimento) values ('joao', null);
-insert into alunos (nome, nascimento) values ('joao', '2007-07-05');
+-- insert
+insert into alunos (nome, idade, nascimento) values ('leo', 10, '2011-03-04');
+insert into alunos (nome, idade, nascimento) values ('juju', 11, '2010-10-08');
+insert into alunos (nome, idade, nascimento) values ('fred', 13, '2008-05-12');
+insert into alunos (nome, idade, nascimento) values ('joao', null, null);
+insert into alunos (nome, idade, nascimento) values ('joao', 14, '2007-07-05');
+-------------------
 
--- selecionar todos os registros
-select * from alunos;
 
--- somente nome
-select nome, nascimento from alunos;
+-- Selecionar todos os campos de uma tabela
+select * 
+from alunos;
 
--- com filtros
-select nascimento from alunos
-where nome = 'fred'
-AND
-nascimento is not null;
+-- Filtrar o resultado por campos nulos e não nulos;
+select nome, idade 
+from alunos 
+where idade is null;
 
--- filtrar os resultados que tenha o campo nascimento nulo
-select* from alunos
-where nascimento is null;
+select nome, idade 
+from alunos 
+where idade is not null; 
 
--- selecione a lista de todos os nomes sem repetição 
-select distinct nome, nascimento from alunos;
+-- Selecionar os registros ignorando os registros duplicados
+select distinct nome 
+from alunos;
 
--- lista de presença - ordenada em ordem alfabética
-select * from alunos 
+-- Ordenar o resultado de uma consulta
+select * 
+from alunos 
 order by nome asc;
 
-select * from alunos
-order by  nascimento desc;
-
--- listar somente dois alunos 
-select * from alunos
-limit 2;
-
--- selecionar 2 alunos p/ receber uma bolsa -- os dois mais velhos que tenham nascimento not null
-select* from alunos
-where nascimento is not null
-order by nascimento 
-limit 2;
+-- Limitar o número de registros retornado por uma consulta
+select * 
+from alunos 
+limit 3;
